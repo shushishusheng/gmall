5fb256245971aba817c0b2158153204c88ad01a3
shushi <excellent_people@163.com>
2019-12-27 19:13:37 +0800

    gmall-api的pojo类和userservice接口

---
+++ gmall-api/src/main/java/cn/excellentpeople/gmall/service/UserService.java
@@ -0,0 +1,17 @@
+package cn.excellentpeople.gmall.service;
+
+
+
+import cn.excellentpeople.gmall.bean.UmsMember;
+
+import java.util.List;
+
+/**
+ * @author 舒丑澳
+ * @version 1.0
+ * 类描述:
+ * @date 2019/10/12 11:29
+ **/
+public interface UserService {
+     List<UmsMember> getAllUser();
+}
